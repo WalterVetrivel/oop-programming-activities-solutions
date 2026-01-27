5dc3eaba854450866fd4caf52ee483960f3d5bc3
Walter Selvakumar <waltervetrivel@yahoo.in>
2026-01-27 15:10:20 +0530

    Added Questions

---
+++ activity3-cricket.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+double calculateRating(int runs, int wickets, int catches, int runOuts)
+{
+    // Formula to calculate rating
+}
+
+int main()
+{
+    // Variable declarations
+
+    // Getting inputs for player #1
+
+    // Getting inputs for player #2
+
+    // Calculating the rating for both players
+
+    // Comparing the ratings and printing the man of the match
+    
+    return 0;
+}
